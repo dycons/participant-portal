36659d8387cfdd6b0b418015feccb36bf148dd37
Mic Fok <michaelmfok@gmail.com>
2020-12-09 15:47:58 -0500

    Adds ability to log in

--- participant-portal-react/src/App.js
+++ participant-portal-react/src/App.js
@@ -1,37 +1,58 @@
 /* eslint-disable no-console */
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import * as Keycloak from 'keycloak-js';
 // import Keycloak from './components/IdentityProvider';
 
 function App() {
-  let keycloak = null;
+  const [auth, setAuth] = useState({
+    loggedIn: false,
+    keycloak: null,
+    user: null,
+  });
 
   const init = async () => {
     const initOptions = {
       url: 'http://127.0.0.1:8080/auth',
-      realm: 'dycons',
+      realm: 'dycons-participant-idp',
       clientId: 'react-client',
     };
 
-    keycloak = Keycloak(initOptions);
-    keycloak.init();
+    const tempKeycloak = Keycloak(initOptions);
+    const authed = await tempKeycloak.init();
+    setAuth({
+      ...auth,
+      keycloak: tempKeycloak,
+      loggedIn: authed,
+      user: tempKeycloak.idTokenParsed,
+    });
+    console.log(`Auth is ${authed}`);
+    console.log({ tempKeycloak });
   };
 
   function login() {
-    keycloak.login();
+    auth.keycloak.login();
   }
 
   useEffect(() => {
+    console.log('Use effect');
     init();
-  });
+  }, []);
 
   return (
     <div className="App">
       <header className="App-header">
-        <div role="button" onClick={login} tabIndex="0" onKeyPress={login}>
-          Login
-        </div>
+        {!auth.loggedIn && auth.keycloak && (
+          <div role="button" onClick={login} tabIndex="0" onKeyPress={login}>
+            Login
+          </div>
+        )}
+        {auth.user && (
+          <div>
+            <div>Logged in.</div>
+            <div>{auth.user.email}</div>
+          </div>
+        )}
       </header>
     </div>
   );
