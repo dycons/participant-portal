7b8bffd2ce35c6a730a2312f5c0a130c37fea832
Mic Fok <michaelmfok@gmail.com>
2020-12-09 17:41:32 -0500

    documentation

--- participant-portal-react/src/App.js
+++ participant-portal-react/src/App.js
@@ -4,16 +4,23 @@ import './App.css';
 import IdentityProvider from './components/IdentityProvider';
 
 function App() {
+  // 0. Modify IdentityProvider configuration for host, realm, and client-id
+  // 1. Set up the auth object to match the output of the Identity Provider
   const [auth, setAuth] = useState({
     loggedIn: false,
     keycloak: null,
     user: null,
   });
 
+  // 3 Call keycloak.login() to log into Keycloak
   function login() {
     auth.keycloak.login();
   }
 
+  // 2.1 use useEffect & [] to only invoke the IdP init once
+  // 2.2 use an IIAAF (Immediately Invoked Async Arrow Function) to use await without another
+  //     function
+  // 2.3 call IdentityProvider.init to trigger auth initialization and set to auth
   useEffect(() => {
     (async () => {
       setAuth(await IdentityProvider.init());
