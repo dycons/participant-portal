2400078cc5a71bfa045c5140c60057697c7d88be
Mic Fok <mic.fok@unity3d.com>
2021-01-29 22:19:55 -0500

    Restyles consents with Bootstrap Table

--- participant-portal-react/src/components/Consent.js
+++ participant-portal-react/src/components/Consent.js
@@ -2,14 +2,19 @@ import React from 'react';
 
 export default function Consent(props) {
   const { consent } = props;
+
+  const geneticConsent = consent.genetic_consent
+    ? 'Consent'
+    : 'Does not consent';
+
+  const clinicalConsent = consent.clinical_consent
+    ? 'Consent'
+    : 'Does not consent';
   return (
-    <div>
-      <div>
-        Project&nbsp;
-        <span>{consent.project_application_id}</span>
-      </div>
-      <div>{consent.genetic_consent ? 'Consent' : 'Does not consent'}</div>
-      <div>{consent.clinical_consent ? 'Consent' : 'Does not consent'}</div>
-    </div>
+    <tr>
+      <td>{consent.project_application_id}</td>
+      <td>{geneticConsent}</td>
+      <td>{clinicalConsent}</td>
+    </tr>
   );
 }
--- participant-portal-react/src/components/ProjectConsents.js
+++ participant-portal-react/src/components/ProjectConsents.js
@@ -1,15 +1,25 @@
 import React from 'react';
+import { Card, Table } from 'react-bootstrap';
 import Consent from './Consent';
 
 export default function ProjectConsents(props) {
   const { consents } = props;
 
   return (
-    <div>
+    <Card body>
       <h1>Consents</h1>
-      {consents.map((consent) => (
-        <Consent consent={consent} />
-      ))}
-    </div>
+      <Table striped>
+        <tbody>
+          <tr>
+            <th>Project Application ID</th>
+            <th>Genetic Consent</th>
+            <th>Clinical Consent</th>
+          </tr>
+          {consents.map((consent) => (
+            <Consent consent={consent} />
+          ))}
+        </tbody>
+      </Table>
+    </Card>
   );
 }
